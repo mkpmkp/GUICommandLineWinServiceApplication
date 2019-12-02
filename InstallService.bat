c56604bf7bca93e18b2e285d063ea1053e06b434
Mastushkin Kirill Petrovich <mkp@inbox.ru>
2019-12-02 14:06:06 +0700

    InstallService.bat

--- InstallService.bat
+++ InstallService.bat
@@ -2,6 +2,6 @@
 
 echo Installing Application1 Service
 echo -------------------------------
-C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\InstallUtil /ServiceName="Application1.Service" /DisplayName="Application1 Service" /ConsolePort="500" /Description="Application1" .\Application1.exe
+C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\InstallUtil /ServiceName="Application1.Service" /DisplayName="Application1 Service" /Description="Application1" .\Application1.exe
 echo -------------------------------
 pause
